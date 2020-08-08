2083cc4d9bf3c0b08452628dccb17de20829e1dc
Greg <gregoryllpowell@gmail.com>
2020-08-08 10:39:15 -0700

    August 8, 2020 Update

--- positivity graphs.R
+++ positivity graphs.R
@@ -30,3 +30,50 @@ ggsave(paste0("Tests, Cases, and Positivity "
 ggplot(covid.az, aes(x=Date))+
   geom_line(aes(y=rollmean(New.Inpatient.Change,7,fill=NA)), linetype='solid', color='#003153', size=1.5)+
   theme_bw()
+
+
+rolling.cases=NULL
+rolling.cases$Date <- covid.az$Date
+rolling.cases$under20 <- covid.az$delta.Cases.Under20 / (covid.az$delta.Positives/1000)
+rolling.cases$c20.44 <- covid.az$delta.Cases.20.44 / (covid.az$delta.Positives/1000)
+rolling.cases$c45.54 <- covid.az$delta.Cases.45.54 / (covid.az$delta.Positives/1000)
+rolling.cases$c55.64 <- covid.az$delta.Cases.55.64 / (covid.az$delta.Positives/1000)
+rolling.cases$over65 <- covid.az$delta.Cases.Over65 / (covid.az$delta.Positives/1000)
+rolling.cases <- data.frame(rolling.cases)
+
+ggplot(rolling.cases, aes(x=Date))+
+  geom_line(aes(y=rollmean(under20,7,fill=NA), color='Under 20'), linetype='solid',  size=1.5)+
+  geom_line(aes(y=rollmean(c20.44,7,fill=NA), color='20 - 44'), linetype='solid',  size=1.5)+
+  geom_line(aes(y=rollmean(c45.54,7,fill=NA), color='45 - 54'), linetype='solid',  size=1.5)+
+  geom_line(aes(y=rollmean(c55.64,7,fill=NA), color='55 - 64'), linetype='solid', size=1.5)+
+  geom_line(aes(y=rollmean(over65,7,fill=NA), color='Over 65'), linetype='solid',  size=1.5)+
+  ggtitle('Cases per 1,000 Positives by Age Group - Arizona')+
+  scale_color_manual(values=c('Under 20' = '#DF536B','20 - 44' = '#61d04f','45 - 54' = '#2297e6','55 - 64' = '#28e2e5',
+    'Over 65' = '#cd0bbc'))+
+  labs(color='Age Group')+
+  ylab('Cases/1,000 Positive Tests')+
+  theme_bw()
+
+
+rolling.cases=NULL
+rolling.cases$Date <- covid.az$Date
+rolling.cases$under20 <- covid.az$delta.Cases.Under20 / (covid.az$delta.Tests/1000)
+rolling.cases$c20.44 <- covid.az$delta.Cases.20.44 / (covid.az$delta.Tests/1000)
+rolling.cases$c45.54 <- covid.az$delta.Cases.45.54 / (covid.az$delta.Tests/1000)
+rolling.cases$c55.64 <- covid.az$delta.Cases.55.64 / (covid.az$delta.Tests/1000)
+rolling.cases$over65 <- covid.az$delta.Cases.Over65 / (covid.az$delta.Tests/1000)
+rolling.cases <- data.frame(rolling.cases)
+
+ggplot(rolling.cases, aes(x=Date))+
+  geom_line(aes(y=rollmean(under20,7,fill=NA), color='Under 20'), linetype='solid',  size=1.5)+
+  geom_line(aes(y=rollmean(c20.44,7,fill=NA), color='20 - 44'), linetype='solid',  size=1.5)+
+  geom_line(aes(y=rollmean(c45.54,7,fill=NA), color='45 - 54'), linetype='solid',  size=1.5)+
+  geom_line(aes(y=rollmean(c55.64,7,fill=NA), color='55 - 64'), linetype='solid', size=1.5)+
+  geom_line(aes(y=rollmean(over65,7,fill=NA), color='Over 65'), linetype='solid',  size=1.5)+
+  ggtitle('Cases per 1,000 Tests by Age Group - Arizona')+
+  scale_color_manual(values=c('Under 20' = '#DF536B','20 - 44' = '#61d04f','45 - 54' = '#2297e6','55 - 64' = '#28e2e5',
+                              'Over 65' = '#cd0bbc'))+
+  labs(color='Age Group')+
+  ylab('Cases/1,000 Tests')+
+  ylim(0,200)+
+  theme_bw()
